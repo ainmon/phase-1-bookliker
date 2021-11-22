414aaf3f3d5e233d3d14af47092ad4b475fc60af
Adam <inmon.adam@yahoo.com>
2021-11-21 21:29:59 -0600

    commit tree e06050a23ef96f6bf689ea8e3891c3a3631d6c63

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('http://localhost:3000/books')
+    .then(r => r.json())
+    .then(obj => obj.map(d => renderBooks(d)))
+
+});
+
+function renderBooks(book){
+    let list = document.getElementById('list')
+    let li = document.createElement('li')
+
+    let title = book.title
+    li.append(title)
+    list.append(li)
+
+    li.addEventListener('click', (e) => {
+        let panel = document.getElementById('show-panel')
+        let p = document.createElement('p')
+        let image = document.createElement('img')
+
+        panel.innerHTML = ''
+
+        image.src = e.target.img_url
+        p.textContent = e.target.description
+
+        panel.append(p, image)
+
+
+    })
+
+}
+
